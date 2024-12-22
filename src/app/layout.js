a210be0f20116d168fac752a9110a551b772c17f
erdemmgunal <65365648+erdemmgunal@users.noreply.github.com>
2024-12-22 16:00:20 +0300

    Update game metadata and enhance question clarity in games.json

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,8 +7,9 @@ const notoSerif = Noto_Serif({
 });
 
 export const metadata = {
-  title: " Hakkı Aranıyor - Mystery Puzzle Game",
-  description: "Explore intriguing mystery puzzles in  Hakkı Aranıyor",
+  title: " Hakkı Aranıyor - Gizemli Bulmaca Oyunu",
+  description: "Hakkı Aranıyor'da ilgi çekici gizemli bulmacaları keşfedin.",
+  "keywords": "bulmaca, durum bulmacası, gizemli bulmaca, durum bulmacaları, durum bulmacası oyunu, durum bulmacası oyunları, durum bulmacası oyna, durum bulmacası çöz, durum bulmacası çözümleri, durum bulmacası örnekleri, durum bulmacası oyunu nasıl oynanır, durum bulmacası oyunu kuralları, durum bulmacası oyunu hakkında, durum bulmacası oyunu hakkında bilgi, durum bulmacası oyunu oyna, durum bulmacası oyunu çöz, durum bulmacası oyunu çözümleri, durum bulmacası oyunu örnekleri, durum bulmacası oyunu nasıl oynanır, durum bulmacası oyunu kuralları, durum bulmacası oyunu hakkında, durum bulmacası oyunu hakkında bilgi, durum bulmacası oyunu oyna, durum bulmacası oyunu çöz, durum bulmacası oyunu çözümleri, durum bulmacası oyunu örnekleri, durum bulmacası oyunu nasıl oynanır, durum bulmacası oyunu kuralları, durum bulmacası oyunu hakkında, durum bulmacası oyunu hakkında bilgi, durum bulmacası oyunu oyna, durum bulmacası oyunu çöz, durum bulmacası oyunu çözümleri, durum bulmacası oyunu örnekleri, durum bulmacası oyunu nasıl oynanır, durum bulmacası oyunu kuralları, durum bulmacası oyunu hakkında, durum bulmacası oyunu hakkında bilgi, durum bulmacası oyunu oyna, durum bulmacası oyunu çöz, durum bulmacası oyunu çözümleri, durum bulmacası oyunu örnekleri, hakkı aranıyor"
 };
 
 export default function RootLayout({ children }) {
